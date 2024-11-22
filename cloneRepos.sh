0b249169ffb8bb906e7b36a68f6a04a4f1a15ad5
Justin Jordan <justinjordan1978@gmail.com>
2024-11-21 20:02:18 -0800

    Update ignore for sdk

--- cloneRepos.sh
+++ cloneRepos.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 git clone https://github.com/RaspberryPi/pico-examples --recurse-submodules
-git clone https://github.com/RaspberryPi/pico-sdk --recurse-submodules
 git clone -b smp https://github.com/FreeRTOS/FreeRTOS-Kernel --recurse-submodules
